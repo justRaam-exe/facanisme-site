256ac6560d9ab50b93b61811eef8486540b1f49d
buramanta <sayapinterbuatakun@gmail.com>
2026-02-15 19:41:55 +0700

    make description looking like typing

--- style/scripting.js
+++ style/scripting.js
@@ -1,1 +1,11 @@
-var i;
+new TypeIt("#typing-desc", {
+  strings: [
+    "Anime Face Recognition System",
+    "Anime Face Analysis System",
+    "Anime Recommendation System Based on Character"
+  ],
+  speed: 70,
+  breakLines: false,
+  loop: true,
+  nextStringDelay: 1600
+}).go();
