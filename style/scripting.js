3bf4f3ae887513548ee9d719c9b54a988267d93f
buramanta <sayapinterbuatakun@gmail.com>
2026-02-18 14:14:14 +0700

    add some animations for navbar scrolling

--- style/scripting.js
+++ style/scripting.js
@@ -9,3 +9,14 @@ new TypeIt("#typing-desc", {
   loop: true,
   nextStringDelay: 1600
 }).go();
+
+window.addEventListener('scroll', () => {
+  const nav = document.getElementById('navbar-sec');
+  if (window.scrollY > 10) {
+    nav.classList.add('bg-[#2B4970]', 'shadow-xl', 'py-2');
+    nav.classList.remove('bg-transparent', 'py-4');
+  } else {
+    nav.classList.remove('bg-[#2B4970]', 'shadow-xl', 'py-2');
+    nav.classList.add('bg-transparent', 'py-4');
+  }
+});
